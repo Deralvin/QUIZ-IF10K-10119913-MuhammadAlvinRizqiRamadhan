e6d174b7f299e7b8e3a9f6f1db7c790e53a1cca4
Dilan <ramadhanrizqi21@gmail.com>
2020-11-27 20:30:14 +0700

    First Code

---
+++ src/quiz/if10k/pkg10119913/muhammadalvinrizqiramadhan/QUIZIF10k10119913MuhammadAlvinRizqiRamadhan.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz.if10k.pkg10119913.muhammadalvinrizqiramadhan;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class QUIZIF10k10119913MuhammadAlvinRizqiRamadhan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
